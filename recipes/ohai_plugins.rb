ebe736d8604d00116c26a9275d1817043398bd05
Modcloth Operations <ops@ops-workstation-1.local>
2012-09-25 16:46:00 -0700

    Add memory plugin for smartos

--- recipes/ohai_plugins.rb
+++ recipes/ohai_plugins.rb
@@ -5,4 +5,11 @@ template "#{node['ohai']['plugin_path']}/cpu.rb" do
   mode 0755
 end
 
+template "#{node['ohai']['plugin_path']}/memory.rb" do
+  source "plugins/memory.rb.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
 include_recipe "ohai"
