911ea56bf3868534f6e74cf28e64c50b1819530d
Ben Marini <bmarini@gmail.com>
2012-11-05 09:54:26 -0800

    Bump version to 0.0.5

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "ops@modcloth.com"
 license          "Apache 2.0"
 description      "SmartOS specific Chef mods"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.4"
+version          "0.0.5"
 
 depends "ohai"
