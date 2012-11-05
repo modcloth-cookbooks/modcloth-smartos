5dac142b7c8828a1802ebc2366c6fc4052d3f429
Seth Kingry <s.kingry@modcloth.com>
2012-11-05 08:59:35 -0800

    Kudos to Christopher Horrell for catching this typos... I still hate SmartOS and Joyent for making me do this.

--- recipes/link_awk.rb
+++ recipes/link_awk.rb
@@ -1,6 +1,6 @@
 case node['platform']
 when 'smartos'
-  link "/opt/local/bin/grep" do
+  link "/opt/local/bin/awk" do
     to "/opt/local/gnu/bin/awk"
     only_if { File.exists?("/opt/local/gnu/bin/awk") }
   end
