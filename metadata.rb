e6c0e66dadd923fd63468527f06982e8369a5812
Stephen Lauck <stpehen.lauck@gmail.com>
2013-01-03 14:25:52 -0800

    [CHEF-3446] refactor user creation/unlocking support for smartos

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "ops@modcloth.com"
 license          "Apache 2.0"
 description      "SmartOS specific Chef mods"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.5"
+version          "0.0.6"
 
 depends "ohai"
